3919ac2d16c3331172c4eaa758ae73103afe490f
volokads <hluhovspg@gmail.com>
2021-10-17 00:48:42 +0300

    записав одну картку

--- src/js/apiServise.js
+++ src/js/apiServise.js
@@ -3,5 +3,8 @@ export default {
     page: 1,
     async fetchImages(imageName) {
 const response = await fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.imageName}&page=${this.page}&per_page=12&key=23569558-943bf7c3d65c4197ad4bffe73`)
-    return await response.json()}
+    console.log(response.json());
+        return await response.json()
+    }
+    
 }
